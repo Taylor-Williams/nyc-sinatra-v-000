1675f71154868e72ec9351baa51166cf0af35ca2
Taylor Williams <taylor@thewilliams.net>
2018-08-30 23:04:58 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,30 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    binding.pry
+    redirect :"figures/#{@figure.id}"
+  end
+
+  post '/figures/:id/edit' do
+    redirect :"figures/#{@figure.id}"
+  end
 end
