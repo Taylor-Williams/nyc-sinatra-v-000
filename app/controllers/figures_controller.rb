0c006a2187d8cfca26d96db98123d999de87dc8b
Taylor Williams <taylor@thewilliams.net>
2018-08-31 00:09:07 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -22,6 +22,26 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     binding.pry
+    @figure = Figure.create(name: params[:figure][:name])
+    if params[:figure][:titles][:ids]
+      @figure.title_ids = params[:figure][:titles][:ids]
+    end
+    if !params[:figure][:titles][:new].empty?
+      @figure.titles << Title.find_or_create_by(name: params[:figure][:titles][:new])
+    end
+    if !params[:figure][:genres].empty?
+      params[:figure][:genres][:ids] = [] if !params[:figure][:genres][:ids]
+      if !params[:figure][:genres][:names].empty?
+        params[:figure][:genres][:names].split(",").map!(&:strip).each do |name|
+          genre = Genre.create(name: name)
+          params[:figure][:genres][:ids] << genre.id
+        end
+      end
+      params[:figure][:genres][:ids].each do |genre_id|
+        @figure.figure_genres.create(genre_id: genre_id)
+      end
+    end
+    @figure.save
     redirect :"figures/#{@figure.id}"
   end
 
